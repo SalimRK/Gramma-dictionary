d02cbe98a952c91fed3581d89f91a801688a07e3
CyberBow <rizksalimrizk@gmail.com>
2023-02-27 19:56:50 +0200

    fix spelling mestakes

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import customtkinter as ctk
-import enchant
+from spellchecker import SpellChecker
 from PIL import Image
 
-d = enchant.Dict("en_US")
+spell = SpellChecker()
 
 # set ctk default colors
 ctk.set_default_color_theme("green.json")
@@ -10,21 +10,22 @@ ctk.set_default_color_theme("green.json")
 # create root window and set name
 root = ctk.CTk()
 root.title("Gramma Dictionary")
+root.iconbitmap("image/GD.ico")
 root.minsize(1140, 500)
 root.resizable(False, False)
 
 
 def fix_spelling(text):
-    words = text.split(" ")
-    for i, word in enumerate(words):
-        if not d.check(word):
-            suggestions = d.suggest(word)
-            if len(suggestions) > 0:
-                words[i] = suggestions[0]
+    paragraph = text
 
-    corrected_text = " ".join(words)
-    print(corrected_text)
-    return corrected_text
+    misspelled = spell.unknown(paragraph.split())
+
+    for word in misspelled:
+        print("Correcting spelling: {}".format(word))
+        paragraph = paragraph.replace(word, spell.correction(word))
+
+    return paragraph
+
 
 
 def process():
@@ -39,11 +40,9 @@ def process():
 
 side_frame = ctk.CTkFrame(root, height=620)
 
-
 GDimage = ctk.CTkImage(dark_image=Image.open("image/Gramma Dictionary.png"), size=(200, 100))
 appImg = ctk.CTkLabel(side_frame, image=GDimage, text="")
 appImg.grid(row=0, column=0, padx=5, pady=5)
-
 
 outputText = ctk.CTkTextbox(root, state="disabled", width=900, height=500)
 inputText = ctk.CTkTextbox(root, width=800, height=100)
